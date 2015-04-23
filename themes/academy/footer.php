784b2b93333e696d02ccf31f9d5a8a2b2860a49d
ck.oconnell@gmail.com <ck.oconnell@gmail.com>
2015-04-23 12:21:24 -0700

    Mobile Footer Tweaking

--- themes/academy/footer.php
+++ themes/academy/footer.php
@@ -3,35 +3,81 @@
 
 <div class="footer">
 	<div class="mainContentWrap footerContent">
-		<div class="footerCol leftPad">
+
+		<!-- Sponsor Block Start -->
+		<div class="sponsorWrap">
+			<div class="sponsorCatWrap skypilot">
+				<h3 class="sponsorCatTitle">Sky Pilot</h3>
+				<a href="#" class="sponsor hive" target="_blank"></a>
+			</div>
+			<div class="sponsorCatWrap chief">
+				<h3 class="sponsorCatTitle">Chief</h3>
+				<a href="#" class="sponsor lasportiva" target="_blank"></a>
+				<a href="#" class="sponsor canwest" target="_blank"></a>
+			</div>
+			<div class="sponsorCatWrap bluffs">
+				<h3 class="sponsorCatTitle">Bluffs</h3>
+				<a href="#" class="sponsor scarpa" target="_blank"></a>
+				<a href="#" class="sponsor valhalla" target="_blank"></a>
+				<a href="#" class="sponsor petzl" target="_blank"></a>
+				<a href="#" class="sponsor flashed" target="_blank"></a>
+				<a href="#" class="sponsor maxim" target="_blank"></a>
+				<a href="#" class="sponsor whistlerbrewing" target="_blank"></a>
+			</div>
+			<div class="sponsorCatWrap supplier">
+				<h3 class="sponsorCatTitle">Supplier</h3>
+				<a href="#" class="sponsor mec" target="_blank"></a>
+				<a href="#" class="sponsor sandman" target="_blank"></a>
+				<a href="#" class="sponsor innate" target="_blank"></a>
+				<a href="#" class="sponsor cliff" target="_blank"></a>
+			</div>
+			<div class="sponsorCatWrap media">
+				<h3 class="sponsorCatTitle">Media</h3>
+				<a href="#" class="sponsor straight" target="_blank"></a>
+				<a href="#" class="sponsor alpinist" target="_blank"></a>
+				<a href="#" class="sponsor mountainculture" target="_blank"></a>
+				<a href="#" class="sponsor mountainlife" target="_blank"></a>
+				<a href="#" class="sponsor gripped" target="_blank"></a>
+			</div>
+			<div class="sponsorCatWrap beneficiary">
+				<h3 class="sponsorCatTitle">Beneficiary</h3>
+				<a href="#" class="sponsor sas" target="_blank"></a>
+				<a href="#" class="sponsor climbersaccess" target="_blank"></a>
+			</div>
+		</div>
+
+
+		<!-- Sponsor Block End -->
+
+		<div class="footerColLogo leftPad">
 			<h1>Arc'teryx<br>Climbing<br>Academy</h1>
 			<h2>Squamish, BC<br>July 10-12, 2015</h2>
 			<p>In Partnership With:</p>
-			<img class="seatoskylogo" src="../wp-content/uploads/2015/04/seatoskylogo.png">
+			<img class="seatoskylogo" src="../wp-content/uploads/2015/04/seatoskylogoWhite.png">
 			<div class="academyLogo"><img src="../wp-content/uploads/2015/04/academyLogo.png" /></div>
 		</div>
 		<div class="mobileClear"></div>
 		<div class="footerCol">
 			<ul class="topMargin">
-				<li>Event Schedule</li>
-				<li>Getting Here</li>
-				<li>Venue Map</li>
-				<li>Contact Us</li>
+				<li><a href="#">Event Schedule</a></li>
+				<li><a href="#">Getting Here</a></li>
+				<li><a href="#">Venue Map</a></li>
+				<li><a href="#">Contact Us</a></li>
 				<br/>
-				<li>About</li>
-				<li>Latest News</li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Latest News</a></li>
 			</ul>
 		</div>
-		<div class="mobileClear"></div>
+		<!-- <div class="mobileClear"></div> -->
 		<div class="footerCol">
 			<ul class="topMargin">
-				<li>Clinics & Workshops</li>
-				<li>Sunset Speaker & Music Series</li>
-				<li>Trade Fair & Demo Gear</li>
-				<li>Fun & Free Events</li>
-				<li>Educational Seminars</li>
-				<li>The Hive Zone</li>
-				<li>Attending Athletes</li>
+				<li><a href="#">Clinics & Workshops</a></li>
+				<li><a href="#">Sunset Speaker & Music Series</a></li>
+				<li><a href="#">Trade Fair & Demo Gear</a></li>
+				<li><a href="#">Fun & Free Events</a></li>
+				<li><a href="#">Educational Seminars</a></li>
+				<li><a href="#">The Hive Zone</a></li>
+				<li><a href="#">Attending Athletes</a></li>
 			</ul>
 		</div>
 	</div>
