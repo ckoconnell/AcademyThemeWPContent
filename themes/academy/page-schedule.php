98ef2713d9258231ddad9af4cbd54bd5d358860d
ck.oconnell@gmail.com <ck.oconnell@gmail.com>
2015-04-24 14:39:39 -0700

    Schedule Page

--- themes/academy/page-schedule.php
+++ themes/academy/page-schedule.php
@@ -7,16 +7,201 @@
 <?php get_header(); ?>
 
 <div id="content">
-
-
-
     <div class="pageContent">
         <div class="pageHead clinicPage">
             <h1>Event<br>Schedule</h1>
         </div>
+        <div class="scheduleWrap">
+            <div class="dayWrap friday">
+                <h1>Friday<br><span class="dateText">July 10, 2015</span></h1>
+                <div class="gondolaWrap">
+                    <h1>Sea To Sky Activities</h1>
+                    <div class="eventWrap sunset">
+                        <h3>Sunset Speaker Series</h3>
+                        <p class="time">6:00pm - 7:45pm</p>
+                        <h1>Women's Climb Night</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap sunset">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">8:00pm - 9:00pm</p>
+                        <h1>Sunset Yoga</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap sunset">
+                        <h3>Music Series</h3>
+                        <p class="time">8:00pm - Close</p>
+                        <h1>Live: The Boom Booms</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                </div>
+            </div>
+            <div class="dayWrap Saturday">
+                <h1>Saturday<br><span class="dateText">July 11, 2015</span></h1>
+                <div class="valleyWrap">
+                    <h1>Valley Activities</h1>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">8:00am - 9:00am</p>
+                        <h1>Pancake Breakfast</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">8:00am - 3:00pm</p>
+                        <h1>SAS/CASB Trail Build Day</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">9:00am - 4:00pm</p>
+                        <h1>Trade Fair & Demo Gear</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap clinics">
+                        <h3>Clinics & Workshops</h3>
+                        <p class="time">9:00am - 3:00pm</p>
+                        <h1>Clinics & Workshops</h1>
+                        <p>All Clincis will meet at the Trade Fair for Registration</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">12:00pm - 2:00pm</p>
+                        <h1>Tacos & Beer</h1>
+                        <p>Presented by Mags 99 & Whistler Brewery</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">1:30pm - 2:30pm</p>
+                        <h1>Maxim Tug Of War</h1>
+                        <p>Registration at Maxim text during the day</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">3:00pm - 4:00pm</p>
+                        <h1>Dodge Ball</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                </div>
+                <div class="gondolaWrap">
+                    <h1>Sea To Sky Activities</h1>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">10:00am - 3:00pm</p>
+                        <h1>Sea To Sky Trail Build Day (ARC)</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap clinics">
+                        <h3>Clinics & Workshops</h3>
+                        <p class="time">9:00am - 3:00pm</p>
+                        <h1>Clinics & Workshops</h1>
+                        <p>All Clincis will meet at the Trade Fair for Registration</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">10:00am - 3:00pm</p>
+                        <h1>Morning Yoga</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap seminars">
+                        <h3>Educational Seminars</h3>
+                        <p class="time">10:30am - 3:30pm</p>
+                        <h1>Seminars</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap sunset">
+                        <h3>Keynote</h3>
+                        <p class="time">6:00pm - 7:30pm</p>
+                        <h1>Keynote & Film Showing</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap sunset">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">8:00pm - 9:00pm</p>
+                        <h1>Sunset Yoga</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap sunset">
+                        <h3>Music Series</h3>
+                        <p class="time">8:00pm - 9:30pm</p>
+                        <h1>Jon & Roy Performance</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="dayWrap sunday">
+                <h1>Sunday<br><span class="dateText">July 12, 2015</span></h1>
+                <div class="valleyWrap">
+                    <h1>Valley Activities</h1>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">8:00am - 9:00am</p>
+                        <h1>Pancake Breakfast</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap clinics">
+                        <h3>Clinics & Workshops</h3>
+                        <p class="time">9:00am - 3:00pm</p>
+                        <h1>Clinics & Workshops</h1>
+                        <p>All Clincis will meet at the Trade Fair for Registration</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">12:00pm - 2:00pm</p>
+                        <h1>Tacos & Beer</h1>
+                        <p>Presented by Mags 99 & Whistler Brewery</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">12:00pm - 2:00pm</p>
+                        <h1>The Hive Dyno Comp</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                </div>
+
+                <div class="gondolaWrap">
+                    <h1>Sea To Sky Activities</h1>
+                    <div class="eventWrap clinics">
+                        <h3>Clinics & Workshops</h3>
+                        <p class="time">9:00am - 3:00pm</p>
+                        <h1>Clinics & Workshops</h1>
+                        <p>All Clincis will meet at the Trade Fair for Registration</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap">
+                        <h3>Fun & Free Events</h3>
+                        <p class="time">10:30am - 11:30pm</p>
+                        <h1>Morning Yoga </h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap seminars">
+                        <h3>Educational Seminars</h3>
+                        <p class="time">10:30am - 3:30pm</p>
+                        <h1>Seminars</h1>
+                        <a href="#">› More Info</a>
+                    </div>
+                    <div class="eventWrap sunset">
+                        <h3></h3>
+                        <p class="time">5:00pm - 7:00pm</p>
+                        <h1>Volunteer Dinner</h1>
+                        <p>@ the Sea To Sky Gondola</p>
+                        <a href="#">› More Info</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
 
     </div>
-
     <div style="clear:both;"></div>
 </div>
 <?php get_footer(); ?>
