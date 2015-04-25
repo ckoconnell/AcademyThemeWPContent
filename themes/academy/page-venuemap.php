5d7e851cc7b7619db13f667af1583373c7488ddd
ck.oconnell@gmail.com <ck.oconnell@gmail.com>
2015-04-24 22:40:11 -0700

    Venue Map Basic Structure

--- themes/academy/page-venuemap.php
+++ themes/academy/page-venuemap.php
@@ -14,7 +14,13 @@
         <div class="pageHead clinicPage">
             <h1>Venue Map</h1>
         </div>
+        <div class="venuemapWrap">
+            <div class="mapImage">
+                <img src="http://localhost/climbingacademydev/wp-content/uploads/2015/04/Aerial-Upper-Shannon-Oct-13.jpg" />
+                <div class="mapMarker mapGondolaBase">Gondola Base</div>
+            </div>
 
+        </div>
     </div>
 
     <div style="clear:both;"></div>
