3c3ba25514fa22593722dfc38c23a38e1cb78a67
ck.oconnell@gmail.com <ck.oconnell@gmail.com>
2015-04-25 22:03:18 -0700

    Getting Here Page - Map + Plugin

--- themes/academy/page-gettinghere.php
+++ themes/academy/page-gettinghere.php
@@ -12,10 +12,16 @@
         <div class="pageHead gettingherePage">
             <h1>Getting Here</h1>
         </div>
-        <div class="introWrap">Intro, Link to Venue Map, Contact Info, Accommodations, external tourism links</div>
-        <div class="googleMapWrap">Google Map</div>
+        <div class="introWrap">
+            <p>Intro, Link to Venue Map, Contact Info, Accommodations, external tourism links, driving times table, </p>
+        </div>
+        <div style="clear:both;"></div>
+        <div class="googleMapWrap">
+            <?php echo do_shortcode('[mapsmarker layer="1"]'); ?>
+        </div>
+        <div style="clear:both;"></div>
         <div class="listingWrap">
-            Listing Wrap (columns)
+
         </div>
 
     </div>
